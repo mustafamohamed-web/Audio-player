00f0a02497cb490b0dc7ce9295fb56b5c9d6ad95
mustafa mohammed <mustafamohammed@mustafas-MacBook-Pro.local>
2022-02-08 12:28:44 +0000

    font-awesome-added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
+import Song from "./components/Song";
+import Player from "./components/Player";
+
 function App() {
   return (
     <div className="App">
       <h1>Music Player</h1>
+      <Player />
+      <Song />
     </div>
   );
 }
---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Song = () => {
+  return (
+    <div>
+      <h1>Song</h1>
+    </div>
+  );
+};
+
+export default Song;
